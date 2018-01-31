e10ba0d0439a1a455f894ff97ad4d0b62a67aaa1
thewizardofroz <rozzie2@hotmail.co.uk>
2018-01-31 16:19:55 +0000

    started flask app integrated joao's html, but everything is dummy or dead end for now

---
+++ spam/README.txt
@@ -0,0 +1,13 @@
+-install with venv in dice
+    source .venv/bin/activate
+    pip3 install --user flask
+    pip3 install --user --editable .
+    add export FLASK_APP=spam
+    and export FLASK_DEBUG=true
+    to end of activate script
+
+-run
+    source .venv/bin/activate
+    python3 -m flask run
+    
+you also need to have the echo server running for the button to work because sockets
---
+++ spam/spam/spam.py
@@ -0,0 +1,107 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import socket
+from sys import argv
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , spam.py
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'spam.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('SPAM_SETTINGS', silent=True)
+
+
+#database functions TODO: swtich to sqlalchemy
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+#to avoid piping into sqlite3 directly
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+#g is a is a general purpose variable associated with the current application context.
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+#TODO: match these up with index.html
+@app.route('/', methods=['GET', 'POST'])
+def mail_delivery():
+    error = None
+    if request.method == 'POST':
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 8888))
+        #template expecting list
+        submit = ["ff","mv","mgsd"]
+            #TODO:this wont work anymore
+            #submit = [request.form['slot1'], request.form['slot2'], request.form['slot3'], request.form['slot4'], request.form['slot5']]
+        s.send(', '.join(submit).encode())
+        return render_template('echo_submit.html', submit=submit)
+    #else
+    return render_template('index.html', error=error)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    #else
+    return render_template('index.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('mail_delivery'))
+
+#TODO: need database editing thingymijig
+        #technically extension, but hey, once I get the db working it'll be fine
+#<a class="nav-link" href="#">Status</a>
+#<a class="nav-link" href="#">Settings</a>
+#<a class="nav-link" href="#">Help</a>
+#<a class="nav-link" href="#">Logout</a>
+
+#TODO: add the 'user' things for reporting wrong mail
+#TODO: get a bluetooth connection cycle going - probably
+    #<span class="badge badge-pill badge-success">Robot Connected</span>
+#TODO: get external post requests working
+@app.route('/slots', methods=['GET', 'POST'])
+def receive_http():
+    error=None
+    if request.method == 'POST':
+        return render_template('login.html', error=error)
+    #else
+    return render_template('login.html', error=error)
