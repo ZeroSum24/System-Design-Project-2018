084a55d1a68fbdb5fd9569b8ac7cbf96d584add1
Alex Shand <alexshand4827@gmail.com>
2018-02-09 14:54:00 +0000

    Final docs

--- robot/Controller/thread_decorator.py
+++ robot/Controller/thread_decorator.py
@@ -5,17 +5,32 @@ from functools import wraps
 
 class GenericThread(threading.Thread):
     """Generic thread object, runs the passed target with arguments"""
+
+    # Threads spawned with daemon = True don't count towards the main thread's
+    # child count. This means if the main thread exits (normally or with an
+    # execption) the interpreter will die immedatly and bring these threads down
+    # with it.
+    daemon = True
+
     def __init__(self, target, args, kwargs):
         threading.Thread.__init__(self)
         self._target = target
         self._args = args
         self._kwargs = kwargs
+
     # Run is called in a seperate thread when the object's start() method is
     # called
     def run(self):
         self._target(*self._args, **self._kwargs)
 
-# Decorator: use with @thread on a callable
+# Decorators in python nearly implement the decorator pattern (See
+# Wikipedia). A python decorator is a function that accepts a function as a
+# parameter and returns a function, generally the returned function calls the
+# original function as well as doing some background work. In this case the
+# returned function when called will call the original function through the
+# GenericThread class above then return the thread object created. The intention
+# is for the thread object to be passed back to the caller so they can call
+# .join() on it to make the function call blocking if desired
 def thread(func):
     """Pushes the decorated callable onto a background thread"""
     # Prevents the decorator from corrupting the wrapped function's metadata
