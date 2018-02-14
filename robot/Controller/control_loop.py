9df5ebf92cd920faa0dc20c16316c30b87c60afa
Grzegorz Wilk <s1504889@sampras.inf.ed.ac.uk>
2018-02-14 13:28:10 +0000

    building blocking and non-blocking structure

--- robot/Controller/control_loop.py
+++ robot/Controller/control_loop.py
@@ -4,6 +4,7 @@ import socket
 import requests
 from sys import argv
 import time
+from queue import Queue
 
 import ev3dev.ev3 as ev3
 import urllib.request as request
@@ -21,17 +22,17 @@ import State
 #--intra-brick-network
 #------------------
 
-#---barcode scanning 
+#---barcode scanning
 #------------------
 
 #[Placeholder ATM] for the third client demo
 
-#---path planning 
+#---path planning
 #------------------
 
 #{(recieves the path plan for the robot (on the output of the Deliver Mail button))}
 
-#---move code 
+#---move code
 #------------------
 
 #{The dispensing code will have to be integrated with line sensing which is managed by the move code currently}
@@ -45,10 +46,14 @@ import State
 
 # temporary bracket-desk mapping, could indicate empty with 0 and have desk number otherwise
 BRACKETS = {1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0}
-# current node number
-CURRENT_POSITION = 0
 
-CHOSEN_PATH = 0
+CURRENT_POSITION = 0 # current node number
+
+CHOSEN_PATH = 0 # this is going to be a dictionary of nodes and angles
+
+STATE_QUEUE = Queue(1)
+
+MOVING_FLAG = False # flag indicating whether the robot is moving
 
 STATE = State.LOADING
 
@@ -69,7 +74,7 @@ def initialize_connection():
 	poll_for_instructions()
 
 def get_current_instruction():
-	global STATE
+	global STATE_QUEUE
 	global BRACKETS
 	global CURRENT_POSITION
 	global TARGET_POSITION
@@ -85,13 +90,41 @@ def poll_for_instructions(): # this can also be an interrupt-based listener, not
 		time.sleep(2) # wait 2s between pooling intervals
 
 def control_loop():
-	pass # somehow loop but choosing the respective loading_loop, delivery_loop etc. based on STATE
+	global STATE
+	while True:
+		if STATE = State.LOADING:
+			STATE = loading_loop() # these are going to be blocking
+		elif STATE = State.DELIVERING:
+			STATE = delivery_loop()
+		elif STATE = State.RETURNING:
+			STATE = return_loop()
+		elif STATE = State.PANIC:
+			STATE = panic_loop()
 
-def choose_target():
-	global TARGET_POSITION
+def loading_loop():
+	# pool for "go-ahead" button
+	pass
+
+def delivery_loop(): # queue-ception to move and back
+	global MOVING_FLAG
+	while True:
+		try:
+			state = STATE_QUEUE.get_nowait()
+			STATE = state
+		except Empty:
+			pass
+
+		if MOVING_FLAG:
+			MOVING_FLAG = False
+			choose_path()
+			move()
+	return State.RETURNING
+
+def choose_path():
 	list_of_paths = []
 	for bracket, table_no in BRACKETS:
 		list_of_paths.append(plan_path(CURRENT_POSITION, table_no))
+	global CHOSEN_PATH
 	CHOSEN_PATH = shortest_path(list_of_paths)
 
 def plan_path():
@@ -100,25 +133,18 @@ def plan_path():
 def shortest_path():
 	pass # returns the shortest path from the path list
 
-def loading_loop():
-	while STATE = State.LOADING
-	# pool for "go-ahead" button
+@thread
+def move():
+	pass # move, junction, dispense
+
+def panic_loop():
 	pass
 
-def delivery_loop():
-	while STATE = State.DELIVERING # move, dispense, return to reception
-	pass
-
-def panic_loop()
-	while STATE = State.PANIC
-	pass
-
-def return_loop()
-	while STATE = State.RETURNING # path
+def return_loop():
+	# path
 	pass
 
 
 if __name__ = "__main__":
 	setup_procedure()
 	control_loop()
- 
