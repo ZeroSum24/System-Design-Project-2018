1e02be9b210db723a01ce145fa25733be906a939
Alex Shand <alexshand4827@gmail.com>
2018-02-15 19:28:52 +0000

    Cleaned up data processing, now with actual error checking

--- robot/Slave/Slave.py
+++ robot/Slave/Slave.py
@@ -6,15 +6,11 @@ from thread_decorator import thread
 import rpyc
 from rpyc.utils.server import ThreadedServer
 from queue import Queue
-import socket
 
 def run(*cmd):
     proc = Popen(cmd, universal_newlines=True, stdout=PIPE)
     stdout, stderr = proc.communicate()
     return stdout
-
-# Start a server
-incoming = Queue()
 
 class _Service(rpyc.Service):
     def exposed_reverse(self, string):
@@ -27,30 +23,61 @@ def _server():
     server = ThreadedServer(_Service, port=8888)
     # This blocks, hence the thread
     server.start()
-_server()
 
 def _get_ips():
-    stdout = run('sudo', 'ifconfig')
+    net_dump = run('sudo', 'ifconfig')
     # Generator comprehension to allow lazy evaluation of intermediate results,
     # strip all leading and trailing whitespace from each line
-    lines = (line.strip() for line in stdout.splitlines())
-    # Information is on a line starting with inet. Fields are seperated by whitespace
-    addresses = (line.split()[1:] for line in _lines if line.startswith('inet'))
-    addresses_dicts = []
-    for address in addresses:
-        addresses_dicts.append(dict(map(lambda x: x.split(':'), address)))
-        
-        res = [x for x in addresses_dicts if x['addr'] != '127.0.0.1'][0] # TODO: Do better
-        # Localhost will be included in the previous result, remove it and any duplicate entries
-        #_ips = {line for line in _ips_unfiltered if line != '127.0.0.1'}
-        
-        slave_ip = res['addr']
-        bcast = res['Bcast']
-        
-        run('sudo', 'ping',' -c', '3', '-b', _bcast)
-        stdout = run('sudo', 'arp', '-a')
-        controller_ip = re.match(r'^.*\((.*)\).*$', _stdout).group(1)
+    lines = (line.strip() for line in net_dump.splitlines())
+    # Information is on a line starting with inet (There are two of these, one
+    # is the one we want, the other is from the loopback interface, it is
+    # distinguisable because it contains 127.0.0.1). Fields are seperated by
+    # whitespace
+    inet = [line.split()[1:] for line in _lines if line.startswith('inet') and not '127.0.0.1' in line]
+    # inet should only have one entry
+    if len(inet) != 1:
+        # TODO: Should be an exception so the control loop can catch it, is a
+        # print statement so we can see it
+        print('Expected 1 IP, got {} ({})'.format(len(inet), '|'.join(inet)))
+        # TODO: Blocking for the above print, remove later
+        while True:
+            pass
+    # Convert to dict by spliting on :, ip is under addr, Broadcast is under Bcast
+    addresses = dict(map(lambda x: x.split(':'), inet[0])))
+    slave_ip = addresses['addr']
+    bcast = addresses['Bcast']
+
+    # ping the broardcast address, seems to force arp cache repopulation
+    run('sudo', 'ping',' -c', '1', '-b', _bcast)
+    # Read the arp cache
+    ips_raw = run('sudo', 'arp', '-a').splitlines()
+    # Should only get one line out as there is only one other computer on the
+    # network
+     if len(ips_raw) != 1:
+        # TODO: Should be an exception so the control loop can catch it, is a
+        # print statement so we can see it
+        print('Expected 1 IP, got {} ({})'.format(len(inet), '|'.join(inet)))
+        # TODO: Blocking for the above print, remove later
+        while True:
+            pass
+    # Assuming the line is what we expect the ip should be in brackets
+    match = re.match(r'^.*\((.*)\).*$', ips_raw)
+    if match:
+        # If the match was successful, pull out the ip
+        controller_ip = match.group(1)
         return slave_ip, controller_ip
+    else:
+        # Or die
+        # TODO: Should be an exception so the control loop can catch it, is a
+        # print statement so we can see it
+        print('Unexpected string format: {}'.format(ips_raw))
+        # TODO: Blocking for the above print, remove later
+        while True:
+            pass
+
+# Start a server
+incoming = Queue()
+_server()
 _slave_ip, _controller_ip = _get_ips()
 
 # Attempt a connection to the controller's server TODO: What happens when there
