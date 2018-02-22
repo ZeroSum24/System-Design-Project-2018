d868654c17df4c6b742c1cba729970b8348077bb
Alex-Shand <alexshand4827@gmail.com>
2018-02-22 13:10:20 +0000

    Spelling

--- robot/Controller/dispense.py
+++ robot/Controller/dispense.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from Controller import slave, incomming
+from Controller import slave, incoming
 
 slave.dump(1)
-if incomming.get():
+if incoming.get():
     slave.dump(2)
-    if incomming.get():
+    if incoming.get():
         slave.stop(3)
-        if incomming.get():
+        if incoming.get():
             slave.dump(4)
-            if incomming.get():
+            if incoming.get():
                 slave.dump(5)
