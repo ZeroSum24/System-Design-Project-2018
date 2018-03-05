46b97086302ee1c6ee4e34ab0b92a9da7f144e43
Grzegorz Wilk <s1504889@wigton.inf.ed.ac.uk>
2018-03-05 23:47:32 +0000

    add dispenser stoppage coroutines

--- robot/Slave/dispenser.py
+++ robot/Slave/dispenser.py
@@ -5,6 +5,7 @@ import imp
 from collections import namedtuple
 from double_map import DoubleMap
 from functools import partial
+from coroutine import coroutine
 
 ##### Setup #####
 
@@ -21,75 +22,94 @@ MOTORS = namedtuple('motors', 'dumper slider')(
 
 def _dump_bracket(bracket):
     if bracket == 1:
-        _run_to_dump(0)
+        pos = 0
     elif bracket == 2:
-        _run_to_dump(100)
+        pos = 100
     elif bracket == 3:
-        _run_to_dump(175)
+        pos = 175
     elif bracket == 4:
-        _run_to_dump(260)
+        pos = 260
     elif bracket == 5:
-        _run_to_dump(325)
+        pos = 325
 
-def _stop_bracket(bracket):
-    if bracket == 1:
-        _run_to_stop(54)
-    elif bracket == 2:
-        _run_to_stop(137)
-    elif bracket == 3:
-        _run_to_stop(212)
-    elif bracket == 4:
-        _run_to_stop(298)
+    r = _run_to_dump(pos)
+    r.send(None)
+    r.send(None)
 
-def _base_run_to(pos, in_between_action = None):
+class stop:
+    def __init__(self, bracket):
+        if bracket == 1:
+            self.pos = 54
+        elif bracket == 2:
+            self.pos = 132
+        elif bracket == 3:
+            self.pos = 207
+        elif bracket == 4:
+            self.pos = 286
+        self.__call__()
+
+    def __call__(self):
+        self.r = _run_to_stop(self.pos)
+
+    def go_further(self):
+        self.r.send(None)
+
+@coroutine
+def _base_run_to(pos, in_between_action = None, shifted_return = False):
 
     if in_between_action is None:
         in_between_action = lambda: None
 
-    _motor_setup(MOTORS.slider, pos)
+    _motor_setup(MOTORS.slider, pos, speed = 100)
+    yield
+    if shifted_return:
+        pos -= 70
     in_between_action()
-    _motor_debrief(MOTORS.slider, pos)
+    yield
+    _motor_debrief(MOTORS.slider, pos, speed = 100)
 
     # making sure the motor touches the end
-    MOTORS.slider.run_timed(speed_sp=-100, time_sp=300)
+    MOTORS.slider.run_timed(speed_sp=-100, time_sp=500)
     _wait_for_motor(MOTORS.slider)
+    yield
 
 def _run_to_dump(pos):
     func = partial(_base_run_to, pos, in_between_action = _raise_dumper)
     return func()
 
+def _run_to_stop(pos):
+    func = partial(_base_run_to, pos, in_between_action = _drop_letter, shifted_return = True)
+    return func()
+
+def _motor_setup(motor, pos, speed = 500):
+    motor.stop_action=Motor.STOP_ACTION_HOLD
+    # solving a wierd bug, where the motor doesn't move w/o this line
+    motor.run_timed(speed_sp=500, time_sp=500)
+    motor.run_to_rel_pos(position_sp=pos, speed_sp=speed)
+    _wait_for_motor(motor)
+
+def _motor_debrief(motor, pos, speed = 500):
+    motor.stop_action=Motor.STOP_ACTION_COAST
+    # solving a wierd bug, where the motor doesn't move w/o this line
+    motor.run_timed(speed_sp=-500, time_sp=500)
+    motor.run_to_rel_pos(position_sp=-pos, speed_sp=speed)
+    _wait_for_motor(motor)
+
+## IN-BETWEEN ACTIONS ##
 def _raise_dumper():
     _motor_setup(MOTORS.dumper, 145)
     time.sleep(2) # wait for 2 seconds for the letter to slide out
     _motor_debrief(MOTORS.dumper, 145)
 
-def _run_to_stop(pos):
-    func = partial(_base_run_to, pos, in_between_action = _scan_letter)
-    return func()
-
-def _scan_letter():
-    # to be implemented
-    time.sleep(2)
-
-def _motor_setup(motor, pos):
-    motor.stop_action=Motor.STOP_ACTION_HOLD
-    # solving a wierd bug, where the motor doesn't move w/o this line
-    motor.run_timed(speed_sp=500, time_sp=500)
-    motor.run_to_rel_pos(position_sp=pos, speed_sp=500)
-    _wait_for_motor(motor)
-
-def _motor_debrief(motor, pos):
-    motor.stop_action=Motor.STOP_ACTION_COAST
-    motor.run_to_rel_pos(position_sp=-pos, speed_sp=500)
-    _wait_for_motor(motor)
+def _drop_letter():
+    # shifts slot to one over, to drop letter
+    _motor_debrief(MOTORS.slider, 70, speed = 100)
+########################
 
 def _wait_for_motor(motor):
-    time.sleep(0.1)         # Make sure that motor has time to start
+    time.sleep(0.1) # Make sure that motor has time to start
     while motor.state==["running"]:
         time.sleep(0.1)
 
 def dump(bracket):
     _dump_bracket(bracket)
-
-def stop(bracket):
-    _stop_bracket(bracket)
