efdc5d303ee279bd2d7dfee33fbb64c17e88f9af
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2018-03-05 19:56:06 +0000

    Routes fixed

--- spam/spam/routes.py
+++ spam/spam/routes.py
@@ -12,7 +12,7 @@ from spam.database import init_db
 from spam import db
 from spam.models import Staff, Location, Problem
 from spam import router
-from flask_xmqtt import Mqtt
+from flask_mqtt import Mqtt
 import json
 from threading import Lock
 from time import sleep
