ac9113505ff785d08852ca4eaa40f27374b61ac9
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2018-03-06 20:49:23 +0000

    Changed some OpenCv Scripts for Testing

--- spam/spam/Image_Test/Scripts/simple_thresholding.py
+++ spam/spam/Image_Test/Scripts/simple_thresholding.py
@@ -4,21 +4,30 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import os
+import sys
 
-imgfile = "test-blue.jpg"
-imgloc = "/home/fdsfsdf/Desktop/Image_Test/imgs/fail/"
-pathname = imgloc + imgfile
-img = cv2.imread(pathname,cv2.IMREAD_GRAYSCALE)
+def main(argv):
 
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
-titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
-for i in xrange(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-plt.show()
+   # accepting the image argument
+   if (len(argv) != 1):
+       print("Please add one image argument")
+       sys.exit(2)
+   imgfile = argv[0]
+
+   img = cv2.imread(imgfile,cv2.IMREAD_GRAYSCALE)
+
+   ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+   ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+   ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+   ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+   ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+   titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+   images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+   for i in xrange(6):
+       plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+       plt.title(titles[i])
+       plt.xticks([]),plt.yticks([])
+       plt.show()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
