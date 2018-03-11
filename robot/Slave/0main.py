3a701976b32c0299e04b939ab013315cfa360d8b
Grzegorz Wilk <s1504889@savona.inf.ed.ac.uk>
2018-03-11 02:31:02 +0000

    resolve phantom classification

--- robot/Slave/0main.py
+++ robot/Slave/0main.py
@@ -8,6 +8,7 @@ from subprocess import Popen, PIPE
 from PIL import Image
 import time
 from thread_decorator import thread
+import os
 
 current_slot = 1
 slot_movement = None
@@ -15,19 +16,26 @@ loading = False
 in_automatic = True
 
 def run(*cmd):
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).wait()
     stdout, stderr = proc.communicate()
     return stdout
 
-def camera_picture():
-    #Camera takes a picture using a command_line subprocess
-    run("fswebcam -r 200x150 --no-banner image_sent.jpg")
+# def _camera_picture():
+#     #Camera takes a picture using a command_line subprocess
+#     run("fswebcam -r 200x150 --no-banner image_sent.jpg")
+#
+#     imgpath = "./image_sent.jpg"
+#     with open(imgpath,'rb') as img:
+#         data = img.read();
+#     client.publish("image_processing", payload=data)
+#     # client.publish("image_processing", payload=pickle.dumps(img))
 
+def camera_picture():
+    os.system('./take_photo.sh')
     imgpath = "./image_sent.jpg"
     with open(imgpath,'rb') as img:
         data = img.read();
     client.publish("image_processing", payload=data)
-    # client.publish("image_processing", payload=pickle.dumps(img))
 
 def on_connect(client, userdata,flags, rc):
     # print("Connected with result code "+str(rc))
