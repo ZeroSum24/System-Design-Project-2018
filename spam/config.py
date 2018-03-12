77a202a3ccc433de4deb8cf34f9a4a9be56c4c6a
ZeroSum24 <stephen_waddell@hotmail.co.uk>
2018-03-12 15:03:02 +0000

    Removed trailing whitespace

--- spam/config.py
+++ spam/config.py
@@ -17,4 +17,3 @@ class Config(object):
    # MQTT_PASSWORD = ''  # set the password here if the broker demands authentication
    # MQTT_KEEPALIVE = 5  # set the time interval for sending a ping to the broker to 5 seconds
    # MQTT_TLS_ENABLED = False  # set TLS to disabled for testing purposes
-    
