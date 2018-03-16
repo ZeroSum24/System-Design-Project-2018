2979f2057c681c6b3bd1b994cced17ec47e4ac54
Alex Shand <alexshand4827@gmail.com>
2018-03-16 12:26:46 +0000

    Improved the install script and modified the dispenser code so 00runme.sh is the only file that needs to be executable

--- robot/install.sh
+++ robot/install.sh
@@ -2,29 +2,50 @@
 
 set -eu
 
+err() {
+    echo "$*" 2>&1
+}
+
 if [[ -z ${DEBUG+x} ]]; then
-    debug=echo
+    debug=err
 else
     debug=:
 fi
 
 copy() {
-    expect <<EOF
+    local output=`expect <<"    EOF"
     spawn scp * robot@ev3dev:~/
     expect "robot@ev3dev's password:"
     send "maker\n"
-EOF
+    EOF`
+    $debug $output
+    return $?
+}
+
+get_brick_number() {
+    local directory=$1
+    if [[ "$directory" == "Controller" ]]; then
+        echo 30
+    elif [[ "$directory" == "Slave" ]]; then
+        echo 10
+    else
+        debug "Got: $directory, expected one of Controller, Slave"
+    fi
 }
 
 install() {
-    local number=$1
-    local directory=$2
+    local directory=$1
+    local number=$(get_brick_number)
     read -p "Please plug in Brick $number (Press enter to continue)"
     cd "./$directory"
     printf "%s" "Installing..."
-    until copy >/dev/null 2>&1; do
+    until copy; do
         sleep 5
     done
+    # TODO: Install required libraries
+    ssh robot@ev3dev <<"    EOF"
+    chmod +x ~/00runme.sh
+    EOF
     echo "done"
     cd ../
 }
@@ -36,5 +57,5 @@ echo '| |\___ \|  ___/ /\ \ | |\/| |'
 echo '|_|____) | |  / ____ \| |  | |'
 echo '(_)_____/|_| /_/    \_\_|  |_|'
 
-install 30 Controller
-install 10 Slave
+install Controller
+install Slave
